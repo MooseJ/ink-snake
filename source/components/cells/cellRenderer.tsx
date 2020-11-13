11b5d080fcda4e4c5ab454db45c01295e2aec76c
Mustafa Jebara <moosejebara@gmail.com>
2020-11-12 19:07:09 -0800

    fix indentation

--- source/components/cells/cellRenderer.tsx
+++ source/components/cells/cellRenderer.tsx
@@ -7,18 +7,18 @@ import { toPosition, doPositionsMatch, Position } from '../../utils/positionHelp
 export type GridCellRenderer = (row: number, column: number) => JSX.Element
 
 export const cellRenderer = (snake: Position[], snack: Position): GridCellRenderer => {
-	return (row: number, column: number) => {
-		const currentPosition = toPosition(row, column)
-		const isSnake = snake.find((snakePart) => doPositionsMatch(snakePart, currentPosition))
-		const isSnack = doPositionsMatch(snack, currentPosition)
-		const isEmpty = !isSnack && !isSnake
-		
-		return (
-			<> 
-				{ isSnake && <SnakeCell/> }
-				{ isSnack && !isSnake && <SnackCell/> }
-				{ isEmpty && <EmptyCell/> }      
-			</>
-		)
-	}
+  return (row: number, column: number) => {
+    const currentPosition = toPosition(row, column)
+    const isSnake = snake.find((snakePart) => doPositionsMatch(snakePart, currentPosition))
+    const isSnack = doPositionsMatch(snack, currentPosition)
+    const isEmpty = !isSnack && !isSnake
+    
+    return (
+      <> 
+        { isSnake && <SnakeCell/> }
+        { isSnack && !isSnake && <SnackCell/> }
+        { isEmpty && <EmptyCell/> }      
+      </>
+    )
+  }
 }
--- source/utils/positionHelpers.ts
+++ source/utils/positionHelpers.ts
@@ -2,17 +2,17 @@ import { GRID_SIZE } from '../styles/dimensions'
 import { Direction } from './helpers'
 
 export interface Position {
-	row: number;
-	column: number;
+  row: number;
+  column: number;
 }
 
 export const toPosition = (row: number, column: number): Position => ({
-	row,
-	column
+  row,
+  column
 })
 
 export const doPositionsMatch = (p1: Position, p2: Position) => 
-	p1.column === p2.column && p1.row === p2.row
+  p1.column === p2.column && p1.row === p2.row
 
 export type PositionShifter = { 
   [key in Direction]: (position: Position) => {
@@ -22,20 +22,20 @@ export type PositionShifter = {
 }
 
 export const positionShifter: PositionShifter = {
-	'UP': (position: Position) => toPosition(position.row - 1, position.column),
-	'DOWN': (position: Position) => toPosition(position.row + 1, position.column),
-	'LEFT': (position: Position) => toPosition(position.row, position.column - 1),
-	'RIGHT': (position: Position) => toPosition( position.row, position.column + 1)
+  'UP': (position: Position) => toPosition(position.row - 1, position.column),
+  'DOWN': (position: Position) => toPosition(position.row + 1, position.column),
+  'LEFT': (position: Position) => toPosition(position.row, position.column - 1),
+  'RIGHT': (position: Position) => toPosition( position.row, position.column + 1)
 }
 
 export const getCenter = (size: number): Position => 
-	toPosition(
-		Math.floor(size/2),
-		Math.floor(size/2)
+  toPosition(
+    Math.floor(size/2),
+    Math.floor(size/2)
   )
   
   export const getRandomPosition = () => 
-	toPosition(
-		Math.floor(Math.random() * GRID_SIZE),
-		Math.floor(Math.random() * GRID_SIZE)
-	)
+  toPosition(
+    Math.floor(Math.random() * GRID_SIZE),
+    Math.floor(Math.random() * GRID_SIZE)
+  )
